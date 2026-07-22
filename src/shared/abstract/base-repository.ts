6a0df77a1381b1296f0f1381083b42d31b94f5dc
febriarr <febriardiansyah020227@gmail.com>
2026-07-22 13:33:40 +0700

    feat: add abstract base repository

---
+++ src/shared/abstract/base-repository.ts
@@ -0,0 +1,37 @@
+import { PgTable } from 'drizzle-orm/pg-core';
+import { InferInsertModel, InferSelectModel, SQL } from 'drizzle-orm';
+import { Database, DatabaseTransaction } from '../../database';
+
+export abstract class BaseRepository<TTable extends PgTable> {
+  constructor(
+    protected readonly db: Database,
+    protected readonly table: TTable
+  ) {}
+
+  protected executor(tx?: DatabaseTransaction) {
+    return tx ?? this.db;
+  }
+
+  async create(input: InferInsertModel<TTable>, tx?: DatabaseTransaction): Promise<InferSelectModel<TTable>> {
+    const [row] = (await this.executor(tx)
+      .insert(this.table)
+      .values(input as any)
+      .returning()) as InferSelectModel<TTable>[];
+
+    return row!;
+  }
+
+  async update(
+    where: SQL,
+    input: Partial<InferInsertModel<TTable>>,
+    tx?: DatabaseTransaction
+  ): Promise<InferSelectModel<TTable> | undefined> {
+    const [row] = (await this.executor(tx)
+      .update(this.table)
+      .set(input as any)
+      .where(where)
+      .returning()) as InferSelectModel<TTable>[];
+
+    return row;
+  }
+}
