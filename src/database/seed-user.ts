abd1483c8a0e35009378a2ddb3da29ad955456ac
febriarr <febriardiansyah020227@gmail.com>
2026-08-02 23:01:30 +0700

    feat: add seed users

---
+++ src/database/seed-user.ts
@@ -0,0 +1,36 @@
+import { db } from './client';
+import { users } from './schema';
+import bcrypt from 'bcrypt';
+import dotenv from 'dotenv';
+dotenv.config();
+
+export async function seedUsers() {
+  const hashedPassword = await bcrypt.hash(process.env.SEED_ADMIN_PASSWORD!, 10);
+
+  await db.insert(users).values([
+    {
+      name: 'febri',
+      email: 'superadmin@example.com',
+      phone: '081234567890',
+      password: hashedPassword,
+      role: 'super_admin',
+    },
+    {
+      name: 'Admin',
+      email: 'admin@example.com',
+      phone: '081234567891',
+      password: hashedPassword,
+      role: 'admin',
+    },
+  ]);
+}
+
+seedUsers()
+  .then(() => {
+    console.log('Users seeded successfully');
+    process.exit(0);
+  })
+  .catch(error => {
+    console.error('Error seeding users:', error);
+    process.exit(1);
+  });
